6b77f4597085aff87a7c884b1961c50759e8bbed
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-07-10 22:58:25 -0400

    js commit

---
+++ frist.js
@@ -0,0 +1,7 @@
+for (let num = 1; num <=20; num++){
+  console.log(`${num}x${num} = ${num * num}`) ;
+}
+for (i = 200; i >=0; i-=25){
+  console.log(i);
+
+}
---
+++ second.js
@@ -0,0 +1,3 @@
+for (let num = 1; num <= 20; num++) {
+  console.log(i);
+}
